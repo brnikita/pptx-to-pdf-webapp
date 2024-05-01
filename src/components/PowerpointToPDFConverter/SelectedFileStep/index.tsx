5cedbf185fcc7c3a432c1ffca06231c95481e2eb
brnikita <branikita@gmail.com>
2024-05-01 21:28:50 +0300

    feat: Updated veiw of the pptx "Selected" step

--- src/components/PowerpointToPDFConverter/SelectedFileStep/index.tsx
+++ src/components/PowerpointToPDFConverter/SelectedFileStep/index.tsx
@@ -25,14 +25,19 @@ export const SelectedFileStep: FC<SelectedFileStepProps> = ({ file, setStep }) =
     }
 
     return (
-        <div className="flex flex-col gap-2 rounded-xl bg-white p-6 shadow-md">
+        <div className="flex flex-col gap-3 rounded-xl bg-white p-6 shadow-md">
             <div className="flex w-full flex-col gap-1 rounded-lg border border-gray-300 p-3 text-center">
                 <p className="text-lg font-semibold text-gray-800">{file.name}</p>
                 <p className="text-sm text-gray-600">{formatBytes(file.size)}</p>
             </div>
-            <div className="flex w-full flex-col gap-1 rounded-lg border border-gray-300 p-3">
-                Convert to PDF <br/> 
-                Best quality, retaining images and other assets.
+            <div className="flex w-full flex-col gap-1 rounded-lg border bg-blue-25 border-blue-200 p-3">
+                <div class="flex items-center">
+                    <input id="convert-to-pdf" type="radio" class="relative bottom-2 w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600" checked />
+                    <label for="convert-option-1" class="block ms-2  text-sm text-blue-800">
+                        Convert to PDF <br/>
+                        Best quality, retaining images and other assets.
+                    </label>
+                </div>
             </div>  
             <div className="flex w-full gap-3">
                 <button
